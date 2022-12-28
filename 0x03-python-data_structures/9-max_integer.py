6e1a1174b1833fe756ba48d9ed5895733679bd9f
Idris Yakub <olayinkayakub@yahoo.com>
2022-12-28 18:09:14 +0100

    list and tuple in python

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -2,8 +2,5 @@
 def max_integer(my_list=[]):
     if my_list is None or len(my_list) is 0:
         return None
-    max = my_list[0]
-    for i in range(len(my_list)):
-        if max < my_list[i]:
-            max = my_list[i]
-    return max
+    my_list.sort()
+    return my_list[-1]
