79227ae69bc5dbc961ab796871664db116dc3de1
Idris Yakub <olayinkayakub@yahoo.com>
2023-01-09 23:35:07 +0100

    inheritance in python

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -3,6 +3,8 @@
 """function that adds a new attribute to an object if its possible, otherwise, raise TypeError"""
 
 def add_attribute(obj, a, v):
+    """function that adds a new attribute to an object if its possible, else raise TypeError"""
+
     res = getattr(obj, "__doc__", None)
     if res is None:
         setattr(obj, a, v)
