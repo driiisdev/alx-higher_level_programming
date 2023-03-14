066ec6e0e28e4bcf60cffbc2436baa666496ec28
Idris Yakub <olayinkayakub@yahoo.com>
2023-03-14 01:20:21 +0100

    second biggest

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
-let args = process.argv.slice(2).map((x) => {
+
+const args = process.argv.slice(2).map((x) => {
   return parseInt(x);
 });
 
 if (args.length <= 1) {
   console.log(0);
 } else {
-  console.log(args.sort((a, b) => {
-    return b - a;
-  })[1]);
+  const maxsecond = args.sort(function (a, b) { return b - a; })[1];
+  console.log(maxsecond);
 }
