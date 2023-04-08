865c1c395aab23457c8ed7dcb93ecbccc4c38a66
idris yakub <olayinkayakub@yahoo.com>
2023-04-08 09:51:58 -0700

    network 1 - python

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
-""" Fetches basic request with urlib"""
-from urllib import request
+""" Python script that fetches https://alx-intranet.hbtn.io/status """
 
-with request.urlopen("https://intranet.hbtn.io/status") as response:
-    r = response.read()
-    print("Body response:\n\t- type: {}\n\t- content: {}\n\t- utf8 content: {}"
-          .format(type(r), r, r.decode('utf-8')))
+if __name__ == "__main__":
+    import urllib.request as request
+    with request.urlopen("https://alx-intranet.hbtn.io/status") as response:
+        if response:
+            html = response.read()
+            print("Body response:")
+            print("\t- type: {}".format(type(html)))
+            print("\t- content: {}".format(html))
+            print("\t- utf8 content: {}".format(html.decode("utf-8")))
--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
-""" Uses requests module -- yay"""
+""" Python script that fetches https://alx-intranet.hbtn.io/status """
 import requests
 
+
 if __name__ == "__main__":
-    response = requests.get('https://intranet.hbtn.io/status')
-    print("Body response:\n\t- type: {}\n\t- content: {}"
-          .format(type(response.text), response.text))
+    response = requests.get("https://alx-intranet.hbtn.io/status")
+    print("Body response:")
+    print("\t- type: {}".format(type(response.text)))
+    print("\t- content: {}".format(response.text))
